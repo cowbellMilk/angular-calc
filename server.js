981e241c23f4f3ee434a0359c4ebf1055e56a71d
cowbellMilk <chukwuebube.glory@gmail.com>
2023-01-26 15:39:48 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static('./dist/ngCalculator'));
+app.use(express.static(__dirname + '/dist/ng-calculator'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ng-calculator'}),
-);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + 'dist/ng-calculator/index.html'));
+});
 
 // Start the app
 app.listen(process.env.PORT || 8080);
